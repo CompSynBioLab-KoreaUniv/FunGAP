fb41c50e4bb8173cec930f216dfb6ef655bd7fb2
mbnmbn00 <mbnmbn00@gmail.com>
2016-11-29 17:58:07 +0900

    fGAP

--- external/bamtools/build/src/api/CMakeFiles/AlgorithmsHeaders.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/AlgorithmsHeaders"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/AlgorithmsHeaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)

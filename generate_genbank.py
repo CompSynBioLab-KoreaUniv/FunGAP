b5ed2e84f8d3841d352ce4d1bbaafd085bbee71b
Byoungnam Min <mbnmbn00@gmail.com>
2021-07-06 15:51:42 -0700

    Minor fix

--- generate_genbank.py
+++ generate_genbank.py
@@ -2,8 +2,7 @@
 
 '''
 Generate Genbank file using GFF3 and annotations
-
-Last updated: May 21, 2021
+--Byoungnam Min. Jul 6, 2021
 '''
 
 import gzip
@@ -11,7 +10,7 @@ import os
 import re
 from urllib.parse import unquote
 from argparse import ArgumentParser
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from datetime import datetime
 
 from Bio import SeqIO
@@ -23,7 +22,7 @@ from Bio.SeqRecord import SeqRecord
 def main():
     '''Main function'''
     argparse_usage = (
-        'generate_genbank.py -f <input_fna> -g <input_gff3> -a <input_faa> ')
+        'generate_genbank.py -f <input_fna> -g <input_gff3> -a <input_faa>')
     parser = ArgumentParser(usage=argparse_usage)
     parser.add_argument(
         '-f', '--input_fna', nargs=1, required=True,
@@ -59,8 +58,9 @@ def main():
     taxonomy = args.taxonomy
 
     # Run functions :) Slow is as good as Fast
+    d_gff3 = parse_gff3(input_gff3)
     generate_genbank(
-        input_fna, input_gff3, input_faa, output_prefix, organism_name,
+        input_fna, d_gff3, input_faa, output_prefix, organism_name,
         data_file_division, taxonomy)
 
 
@@ -87,23 +87,14 @@ def parse_gff_attributes(attribute_string):
 
 def parse_gff3(filename):
     '''A minimalistic GFF3 format parser'''
-    # Yields objects that contain info about a single GFF3 feature.
-    # Supports transparent gzip decompression.
-
     # Parse with transparent decompression
-    gff_info_fields = [
-        'seqid', 'source', 'type', 'start', 'end', 'score', 'strand',
-        'phase', 'attributes']
-    gff_record = namedtuple('GFFRecord', gff_info_fields)
     open_func = gzip.open if filename.endswith('.gz') else open
+    d_gff3 = defaultdict(list)
     with open_func(filename) as infile:
         for line in infile:
             if line.startswith('#'):
                 continue
             parts = line.strip().split('\t')
-            # If this fails, the file format is not standard-compatible
-            assert len(parts) == len(gff_info_fields)
-            # Normalize data
             normalized_info = {
                 'seqid': None if parts[0] == '.' else unquote(parts[0]),
                 'source':
@@ -116,14 +107,12 @@ def parse_gff3(filename):
                     None if parts[6] == '.' else unquote(parts[6]),
                 'phase': None if parts[7] == '.' else unquote(parts[7]),
                 'attributes': parse_gff_attributes(parts[8])}
-            # Alternatively, you can emit the dictionary here,
-            # if you need mutability:
-            # yield normalizedInfo
-            yield gff_record(**normalized_info)
+            d_gff3[parts[0]].append(normalized_info)
+    return d_gff3
 
 
 def generate_genbank(
-        input_fna, input_gff3, input_faa, output_prefix, organism_name,
+        input_fna, d_gff3, input_faa, output_prefix, organism_name,
         data_file_division, taxonomy):
     '''Generate GenBank format'''
     # Output file name
@@ -132,7 +121,6 @@ def generate_genbank(
     # First, import input_fna in dictionary
     d_fna = SeqIO.to_dict(SeqIO.parse(input_fna, 'fasta'))
     d_faa = SeqIO.to_dict(SeqIO.parse(input_faa, 'fasta'))
-
     d_fna_sorted = sorted(
         d_fna.items(),
         key=lambda x: int(re.findall(r'\d+', x[0])[0]))
@@ -140,19 +128,20 @@ def generate_genbank(
     # Make dictionary for CDS
     d_cds = defaultdict(list)
     d_exon = defaultdict(list)
-    for record in parse_gff3(input_gff3):
-        if record.type == 'exon':
-            exon_parent = record.attributes['Parent']
-            d_exon[exon_parent].append(record)
-
-        elif record.type == 'CDS':
-            cds_parent = record.attributes['Parent']
-            d_cds[cds_parent].append(record)
+    for scaffold, records in d_gff3.items():
+        for record in records:
+            if record['type'] == 'exon':
+                exon_parent = record['attributes']['Parent']
+                d_exon[exon_parent].append(record)
+            elif record['type'] == 'CDS':
+                cds_parent = record['attributes']['Parent']
+                d_cds[cds_parent].append(record)
 
     my_seq_records = []
     for scaffold, seq in d_fna_sorted:
         my_seq = Seq(str(seq.seq))
         my_seq_record = SeqRecord(my_seq)
+        my_seq_record.id = scaffold
         my_seq_record.description = '{} {}'.format(organism_name, scaffold)
         date = datetime.today().strftime('%d-%^b-%Y')
         my_seq_record.annotations['date'] = date
@@ -162,22 +151,27 @@ def generate_genbank(
             'Whole genome sequencing project']
         my_seq_record.annotations['taxonomy'] = taxonomy.split('; ')
         my_seq_record.annotations['source'] = organism_name
+        my_seq_record.annotations['molecule_type'] = 'DNA'
 
-        for record in parse_gff3(input_gff3):
-            if scaffold != record.seqid:
-                continue
+        # Put source
+        source_feature_location = FeatureLocation(0, len(seq))
+        source_qualifiers = {
+            'organism': organism_name, 'mol_type': 'genomic DNA'}
+        source_feature = SeqFeature(
+            source_feature_location, type='source',
+            qualifiers=source_qualifiers)
+        my_seq_record.features.append(source_feature)
 
-            my_feature_type = record.type
+        for record in d_gff3[scaffold]:
+            my_feature_type = record['type']
             if my_feature_type == ('exon', 'CDS'):
                 continue
-
             # GFFRecord(seqid='contig1', source='AUGUSTUS', type='gene',
             # start=16942, end=19008, score=0.22, strand='+', phase=None,
             # attributes={'Source': 'braker_Y1:g3308.t1', 'ID': 'Triga_00001'})
-
-            my_start = record.start
-            my_end = record.end
-            my_strand = 1 if record.strand == '+' else -1
+            my_start = record['start']
+            my_end = record['end']
+            my_strand = 1 if record['strand'] == '_' else -1
 
             # Set qualifies for gene
             if my_feature_type == 'gene':
@@ -186,7 +180,7 @@ def generate_genbank(
                 gene_feature_location = FeatureLocation(
                     gene_start, gene_end, strand=my_strand)
                 gene_qualifiers = {}
-                gene_locus_tag = record.attributes['ID']
+                gene_locus_tag = record['attributes']['ID']
                 gene_qualifiers['locus_tag'] = gene_locus_tag
                 gene_feature = SeqFeature(
                     gene_feature_location, type=my_feature_type,
@@ -196,16 +190,17 @@ def generate_genbank(
 
             elif my_feature_type == 'mRNA':
                 sorted_exon_records = sorted(
-                    d_exon[record.attributes['ID']], key=lambda x: x.start)
+                    d_exon[record['attributes']['ID']],
+                    key=lambda x: x['start'])
                 sorted_cds_records = sorted(
-                    d_cds[record.attributes['ID']], key=lambda x: x.start)
-
+                    d_cds[record['attributes']['ID']], key=lambda x: x['start'])
                 # Feature locations
                 # mRNA location is needed to be modified
                 fl_mrna_list = []
                 for exon_record in sorted_exon_records:
                     fl_element = FeatureLocation(
-                        exon_record.start, exon_record.end, strand=my_strand)
+                        exon_record['start'], exon_record['end'],
+                        strand=my_strand)
                     fl_mrna_list.append(fl_element)
 
                 if len(fl_mrna_list) == 1:
@@ -216,7 +211,8 @@ def generate_genbank(
                 fl_cds_list = []
                 for cds_record in sorted_cds_records:
                     fl_element = FeatureLocation(
-                        cds_record.start, cds_record.end, strand=my_strand)
+                        cds_record['start'], cds_record['end'],
+                        strand=my_strand)
                     fl_cds_list.append(fl_element)
 
                 # If fl_cds_list is more than 1 use CompoundLocation
@@ -228,33 +224,27 @@ def generate_genbank(
                 # Qualifier
                 mrna_qualifiers = {}
                 cds_qualifiers = {}
-
-                mrna_locus_tag = record.attributes['ID']
+                mrna_locus_tag = record['attributes']['ID']
                 mrna_qualifiers['locus_tag'] = mrna_locus_tag
-                if record.score:
+                if record['score']:
                     mrna_qualifiers['note'] = 'prediction score=%s' % (
-                        record.score)
-
+                        record['score'])
                 cds_qualifiers['locus_tag'] = mrna_locus_tag
                 # Get phase
                 if my_strand == 1:
-                    phase = int(sorted_cds_records[0].phase) + 1
+                    phase = int(sorted_cds_records[0]['phase']) + 1
                 elif my_strand == -1:
-                    phase = int(sorted_cds_records[-1].phase) + 1
+                    phase = int(sorted_cds_records[-1]['phase']) + 1
                 cds_qualifiers['codon_start'] = phase
                 cds_qualifiers['translation'] = str(d_faa[mrna_locus_tag].seq)
-
                 mrna_feature = SeqFeature(
-                    mrna_feature_location, type='mRNA',
-                    qualifiers=mrna_qualifiers)
-
+                    mrna_feature_location, type='mRNA',                  qualifiers=mrna_qualifiers)
                 cds_feature = SeqFeature(
                     cds_feature_location, type='CDS', qualifiers=cds_qualifiers)
                 # Append my feature to seq_record
                 my_seq_record.features.append(mrna_feature)
                 my_seq_record.features.append(cds_feature)
         my_seq_records.append(my_seq_record)
-
     SeqIO.write(my_seq_records, outfile, 'genbank')
 
 
